14699809cccb5b42bf3ebb30f8cd42538ae77400
trk <blrks@nevermore3.fake>
2021-11-05 18:21:01 +0100

    added PrefixTree test

--- test/test_trie.py
+++ test/test_trie.py
@@ -2,6 +2,7 @@ import unittest
 
 from syllabicator.trie import *
 
+
 class TestNode(unittest.TestCase):
     def test_insert(self):
         n = Node("a")
@@ -10,17 +11,20 @@ class TestNode(unittest.TestCase):
                          "'a': (None, {'x': (None, {'y': (1, set())})})",
                          "Wrong Trie structure after insert")
 
+
     def test_longestMatch(self):
         n = Node("a")
         n.insert("xy")
         self.assertEqual(n.longestMatch("xyz"), "xy", "match not found or incorrect")
         self.assertEqual(n.longestMatch("qwe"), "", "match found where there is none")
 
+
     def test_get(self):
         n = Node("a")
         n.insert("xyz", 5)
         self.assertEqual(n.get("xyz"), 5, "item not found")
         self.assertEqual(n.get("qwe"), None, "nonexistent item found")
+
 
     def test_delete(self):
         n = Node("a")
@@ -33,11 +37,20 @@ class TestNode(unittest.TestCase):
         self.assertRaises(KeyError,
                           lambda x: n.delete(x), "qwe")
 
+
     def test_contains(self):
         n = Node("a")
         n.insert("xy")
         self.assertTrue("xy" in n)
         self.assertFalse("qw" in n)
+
+
+class TestPrefixTree(TestNode):
+    def test_insert(self):
+        n = PrefixTree()
+        n.insert("")
+        self.assertEqual(repr(n), "'': (True, set())",
+                         "Insert of empty word resulted in wrong Trie structure.")
 
 
 if __name__ == "__main__":
